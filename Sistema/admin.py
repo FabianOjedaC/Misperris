cf0e7cc6d39e65f69644c7bb6ae0c8c5cace0421
FabianOjedaC <fabianojedac@gmail.com>
2018-11-26 20:30:14 -0300

    Se agrego el modelo persona y mascota al admin

--- Sistema/admin.py
+++ Sistema/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+class AdminUsuario(admin.ModelAdmin):
+    list_display = ["nombrePersona", "numeroFono"]
+    class Meta:
+        model = Persona
+
+
+admin.site.register(Persona, AdminUsuario)
+
+class AdminMascota(admin.ModelAdmin):
+    list_display = ["nombreMascota", "estadoMascota"]
+    class Meta:
+        model = Mascota
+
+
+admin.site.register(Mascota, AdminMascota)
